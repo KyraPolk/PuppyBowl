c06fc42a436d34b2b4779ae7eaa19fcc30f0ad64
Kyra Polk <kyraambeau@gmail.com>
2023-08-31 11:54:59 -0500

    got the uri to my webpage and added some css

---
+++ JavaScript.js
@@ -0,0 +1,34 @@
+console.log("hello world");//it works, and connected
+
+//the first thing i need to do link the desired element to js using querySelector
+let ul = document.querySelector('ul');//grabbing the ul
+
+//next we need a way to "fetch" the information from our server to our webpage using a function
+//the function should be async to ensure the function returns a promise
+//because we used async, we will need to 'await' two times
+const fetcher = async () =>{
+//this function will fetch a response from the URI
+const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2307/players");
+const json = await response.json();//should return an object
+pupArray = json.data.players;//to get an array
+console.log(pupArray);//outcome is an array of 10 objects containing each puppy info
+//next I want to loop using .map to retrieve the name and breed the name I want in an <h2> and the breed I want in a <p>
+const html = pupArray.map(puppy => {
+  return `
+  <li>
+  <a><h2>${puppy.name}</h2></a>
+  <p>${puppy.breed}</p>
+  </li>
+  `
+  //we still have an array of this information so we need to convert it into a string using .join
+}).join("");
+//still within the same function, we want to set the contents of our ul tag to each result
+ul.innerHTML = html;
+}
+//fetcher();
+
+//now that we've fetched the data from the URI we need to "render" it to the webpage
+function render(){
+  fetcher()
+}//wont see anything until we invoke render function
+render()
