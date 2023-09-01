bfe0cf5ab0830b7fd26ad981028f9be59e5b9ae3
Kyra Polk <kyraambeau@gmail.com>
2023-09-01 13:45:25 -0500

    a few changes/additions

--- JavaScript.js
+++ JavaScript.js
@@ -13,10 +13,11 @@ const json = await response.json();//should return an object
 pupArray = json.data.players;//to get an array
 console.log(pupArray);//outcome is an array of 10 objects containing each puppy info
 //next I want to loop using .map to retrieve the name and breed the name I want in an <h2> and the breed I want in a <p>
+const hash = window.location.hash.slice(1) * 1
 const html = pupArray.map(puppy => {
   return `
   <li>
-  <a href="index.html"><h2>${puppy.name}</h2></a>
+  <a href="#${puppy.id !== hash ? puppy.id : ""}" class= "${puppy.id === hash ? "selected" : ""}"><h2>${puppy.name}</h2></a>
   <p>${puppy.breed}</p>
   </li>
   `
@@ -29,7 +30,10 @@ ul.innerHTML = html;
 
 //now that we've fetched the data from the URI we need to "render" it to the webpage
 function render(){//reusable function to get my changes to the webpage
+
+  //const hash = window.location.hash.slice(1) * 1
   fetcher()
+
 }//wont see anything until we invoke render function
 render()
 
